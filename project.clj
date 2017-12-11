e0980b0192f241bd89414a1bc56d0ecb7fedeb50
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-12-11 00:16:53 -0800

    Add missing deps

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/arrdem/stacks"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.reader "1.1.1"]
+                 [rewrite-clj/rewrite-clj "0.6.0"]])
