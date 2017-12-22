a40432df12f7c6ded241f574b954cf72831b4e41
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-12-21 23:40:47 -0800

    Update deps, lein-git-version

--- project.clj
+++ project.clj
@@ -3,7 +3,18 @@
   :url "https://github.com/arrdem/stacks"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.reader "1.1.1"]
                  [rewrite-clj/rewrite-clj "0.6.0"]
-                 [markdown-clj/markdown-clj "1.0.1"]])
+                 [me.arrdem/commonmark-hiccup "LATEST"]
+                 [me.arrdem/detritus "LATEST"]]
+
+  :plugins [[me.arrdem/lein-git-version "LATEST"]]
+  :git-version {:status-to-version
+                (fn [{:keys [tag version ahead ahead? dirty?] :as git}]
+                  (if (and tag (not ahead?) (not dirty?))
+                    tag
+                    (str tag
+                         (when ahead? (str "." ahead))
+                         (when dirty? "-SNAPSHOT"))))})
