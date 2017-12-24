4bf041f57026c4e934faa7d505c86fe55dc6f305
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-12-23 18:07:32 -0800

    Typo

--- src/stacks/doctree.clj
+++ src/stacks/doctree.clj
@@ -4,5 +4,5 @@
   Articles don't come alone.
   They're the most useful when they come in groups.
   Documentation engines need to work in terms of large filesets of documentation and sources."
-  (:require [stacks.articles :as articles))
+  (:require [stacks.articles :as articles]))
 
