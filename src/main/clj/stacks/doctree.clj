98a81f6f0ca36880f18e3b22c1cf66fea46ddf6e
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-01-04 18:47:30 -0800

    Start trying to refactor everything into tools & project middleware

--- src/main/clj/stacks/doctree.clj
+++ src/main/clj/stacks/doctree.clj
@@ -21,91 +21,12 @@
 (set! *warn-on-reflection* true)
 
 (def default-options
-  {:doc-extensions     #{".md" ".markdown"}
-   :source-extensions  #{".clj" ".cljc"}
+  {:source-extensions  #{".clj" ".cljc"}
    :session-extensions #{".repl"}
    :ignored-patterns   #{#"checkouts"}})
 
-(defn- normalize-patterns
-  "Implementation detail.
-
-  Used to normalize ignored URI patterns to patterns."
-  [patterns?]
-  (->> patterns?
-       (map (fn [p] (if (string? p)
-                      (re-pattern (format ".*?%s$" (string/escape p {\. "\\."})))
-                      (if (pattern? p)
-                        p
-                        (throw (IllegalArgumentException.
-                                (format "Cannot normalize type %s to java.util.regex.Pattern"
-                                        (class p))))))))
-       (into #{})))
-
-(defn- normalize-options
-  "Implementation detail.
-
-  Used to normalize options before we try and work with them."
-  [options]
-  (-> options
-      (update :ignored-patterns normalize-patterns)
-      (update :source-extensions normalize-patterns)
-      (update :session-extensions normalize-patterns)
-      (update :doc-extensions normalize-patterns)
-      (assoc :type ::options)))
-
-(defn uri-ignored?
-  "Given a configuration, return `true` if and only if the given URI matches an ignored pattern."
-  [{patterns :ignored-patterns :as config} ^URI uri]
-  {:pre [(uri? uri)]}
-  (let [uri-str (.toString uri)]
-    (boolean (some #(re-find % uri-str) patterns))))
-
-(defn find-path
-  "Implementation detail.
-
-  Given a File instance, a sequence containing only the given File and its children if any."
-  [options cache ^File path]
-  (when-not (uri-ignored? options (.toURI path))
-    (or (get @cache path)
-        (let [r (when (.exists path)
-                  (cons path
-                        (if (.isDirectory path)
-                          (mapcat (fn [^File entry]
-                                    (if (.isDirectory entry)
-                                      (find-path options cache entry)
-                                      [entry]))
-                                  (map (partial io/file path)
-                                       (.list path))))))]
-          (vswap! cache assoc path r)
-          r))))
-
-(defn- find-project-files
-  "Implementation detail.
-
-  Finds one kind of files in the project structure and indexes them,
-  using a cache to minimize directory traversal.
-
-  Recursively walks all directories listed as source, resource or doc
-  paths, returning sequence of structure all located files and directories.
-
-  Files matching any of the configured `:ignored-patterns` are ignored."
-  [filter-k path-ks result-k options project cache]
-  (let [filter-patterns (get options filter-k)
-        paths           (mapcat project path-ks)]
-    {result-k (->> paths
-                   (map #(if-not (file? %) (io/file %) %))
-                   (mapcat (partial find-path options cache))
-                   (keep (fn [^File f]
-                           (let [uri      ^URI (.toURI f)
-                                 uri-text (.toString uri)]
-                             (when (some #(re-matches % uri-text) filter-patterns)
-                               uri)))))}))
-
 (def find-sources
   (partial find-project-files :source-extensions [:source-paths] :sources))
-
-(def find-docs
-  (partial find-project-files :doc-extensions [:doc-paths] :docs))
 
 (def find-sessions
   (partial find-project-files :session-extensions [:session-paths] :sessions))
--- src/main/clj/stacks/middleware/articles.clj
+++ src/main/clj/stacks/middleware/articles.clj
@@ -1,14 +1,29 @@
 (ns stacks.middleware.articles
   "A Stacks server middleware for rendering articles."
   {:authors ["Reid \"arrdem\" McKenzie <me@arrdem.com>"]
-   :license "https://www.eclipse.org/legal/epl-v10.html"})
+   :license "https://www.eclipse.org/legal/epl-v10.html"}
+  (:require [stacks.middleware.util :refer [find-project-files]]
+            [stacks.tools.articles :as articles]))
 
-(defn articles
-  "Middleware transformer.
+(def default-options
+  {:article-extensions #{".md" ".markdown"}})
+
+(def find-articles
+  (partial find-project-files :doc-extensions [:doc-paths] :articles))
+
+(defn index-articles [options project project-files]
+  (let [{:keys [articles]} project-files]
+    (map articles/markdown->article articles)))
+
+#_(defn wrap-articles
+    "Middleware transformer.
 
   Accepts a function of a Leiningen project which generates a Stacks content tree.
   Returns a new function of a Leiningen project which will also generate article content."
-  [f]
-  ;; FIXME (arrdem 2017-12-29):
-  ;;   Do something.
-  f)
+    ([f]
+     (wrap-articles f default-options))
+    ([f options]
+     ;; FIXME (arrdem 2017-12-29):
+     ;;   Do something.
+     (fn [global-options project]
+       (-> (index-articles (merge global-options options) project) (f)))))
