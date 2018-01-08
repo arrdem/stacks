5c4cd382d50b63566536056a35f592d5c879fa1b
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-01-07 16:54:21 -0800

    Add tests of the doctest language

---
+++ src/test/clj/stacks/tools/doctests_test.clj
@@ -0,0 +1,16 @@
+(ns stacks.tools.doctests-test
+  (:require [stacks.tools.doctests :as dt]
+            [clojure.test :as t]
+            [clojure.java.io :as io]))
+
+(t/deftest parser-test
+  (let [doctests? (dt/parse-doctests (slurp (io/resource "example.doctest")))]
+    (t/is doctests?)
+
+    (doseq [t (:tests doctests?)]
+      (t/is (:input t))
+      (t/testing (format "Testing that %s has populated assertion forms" t)
+        (doseq [a (:assertions t)]
+          (t/is (:input a)))))
+
+    ((dt/compile doctests?))))
