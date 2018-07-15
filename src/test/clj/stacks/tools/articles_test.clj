75eb514135953ddaa68a3bbb15393e144bfc7911
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-07-15 15:37:00 -0700

    Fix the article parsing tests

--- src/test/clj/stacks/tools/articles_test.clj
+++ src/test/clj/stacks/tools/articles_test.clj
@@ -5,4 +5,25 @@
 
 (t/deftest parser-test
   (t/testing "Testing the root example"
-    (t/is (a/markdown->article (slurp (io/resource "example.md"))))))
+    (t/is (a/parse-article a/handle-parse-block
+                           (io/resource "example.md"))))
+
+  (t/testing "Testing the README"
+    (t/is (a/parse-article a/handle-parse-block
+                           (io/file "README.md"))))
+
+  (t/testing "Testing the articles document"
+    (t/is (a/parse-article a/handle-parse-block
+                           (io/file "doc/articles.md"))))
+
+  (t/testing "Testing the doctests document"
+    (t/is (a/parse-article a/handle-parse-block
+                           (io/file "doc/doctests.md"))))
+
+  (t/testing "Testing the projects document"
+    (t/is (a/parse-article a/handle-parse-block
+                           (io/file "doc/projects.md"))))
+
+  (t/testing "Testing the sessions document"
+    (t/is (a/parse-article a/handle-parse-block
+                           (io/file "doc/sessions.md")))))
